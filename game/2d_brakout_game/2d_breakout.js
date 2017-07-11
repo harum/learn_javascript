ced046c7f2488b2f144c0e21e33895efa302583e
Putu Harum Bawa <lightharum.108@gmail.com>
2017-07-11 23:07:12 +0700

    add paddle movement

--- game/2d_brakout_game/2d_breakout.js
+++ game/2d_brakout_game/2d_breakout.js
@@ -8,17 +8,32 @@
   let dy = -2;
   const ballRadius = 10;
 
+  const paddleHeight = 10;
+  const paddleWidth = 75;
+  let paddleX = (canvas.width - paddleWidth) / 2;
+  let rightPressed = false;
+  let leftPressed = false;
+
   const drawBall = function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
     ctx.fillStyle = '#0095DD';
     ctx.fill();
     ctx.closePath();
-  }
+  };
+
+  const drawPaddle = function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+  };
 
   const draw = function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+    drawPaddle();
 
     if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
       dx = -dx;
@@ -27,9 +42,36 @@
       dy = -dy;
     }
 
+    if(rightPressed && paddleX < canvas.width - paddleWidth) {
+      paddleX += 7;
+    } else if(leftPressed && paddleX > 0) {
+      paddleX -= 7;
+    }
+
     x += dx;
     y += dy;
-  }
+  };
+
+  function keyDownHandler(e) {
+    console.log('keyDownHandler');
+    console.log(e.keyCode);
+    if(e.keyCode === 39) {
+      rightPressed = true;
+    } else if (e.keyCode === 37) {
+      leftPressed = true;
+    }
+  };
+
+  function keyUpHandler(e) {
+    if(e.keyCode === 39) {
+      rightPressed = false;
+    } else if (e.keyCode === 37) {
+      leftPressed = false;
+    }
+  };
+
+  document.addEventListener('keydown', keyDownHandler, false);
+  document.addEventListener('keyup', keyUpHandler, false);
 
   setInterval(draw, 10);
 }());
