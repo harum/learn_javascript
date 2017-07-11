e698c759d2619baf6ba8755b416984d51538f949
Putu Harum Bawa <lightharum.108@gmail.com>
2017-07-11 20:08:51 +0700

    learn dom event

--- dom/event.js
+++ dom/event.js
@@ -3,12 +3,18 @@
  * https://dom.spec.whatwg.org/#introduction-to-dom-events
  */
 
-(function(){
+(function myApps(){
   console.log('learn dom event');
+  /*
+   * event will be running in 3 step
+   * CAPTURING_PHASE = 1; ( with { capture: true } parameter )
+   * AT_TARGET = 2;
+   * BUBBLING_PHASE = 3;
+   */
 
   const objEvent = document.getElementById('event');
   const objCapture = document.getElementById('capture');
-  const myCustomEvent = new Event("callMe", { bubbles: true });
+  const myEvent = new Event('callMe', { bubbles: true });
 
   const fn = function fn(e) {
     console.log(e.target, e.currentTarget, e.eventPhase);
@@ -17,6 +23,18 @@
   document.addEventListener('callMe', fn);
   objEvent.addEventListener('callMe', fn);
   objCapture.addEventListener('callMe', fn, { capture: true });
+  objEvent.dispatchEvent(myEvent);
 
-  objEvent.dispatchEvent(myCustomEvent);
+
+  console.log('event delegation');
+  const handleDelegation = function handleDelegation(e) {
+    console.log(e.target, e.currentTarget, e.eventPhase);
+  };
+
+  /*
+   * instead add listener on item list
+   * add the listener on its parent
+   */
+  const parentList = document.getElementById('list');
+  parentList.addEventListener('click', handleDelegation);
 }())
